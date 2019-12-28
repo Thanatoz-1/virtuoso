f552db65055d2959b573cd7298787b387d40de89
Tushar Dhyani <dhyanitushar@gmail.com>
2019-12-28 22:58:23 +0530

    Format changed to yapf

--- virtuso.py
+++ virtuso.py
@@ -7,47 +7,55 @@ import spacy
 
 nlp = spacy.load('en_core_web_sm')
 
+
 class DataFetcher:
     def __init__(self):
-        BASE='Data/'
-        self.tables=os.listdir(BASE)
-        self._data = defaultdict(lambda:[])
+        BASE = 'Data/'
+        self.tables = os.listdir(BASE)
+        self._data = defaultdict(lambda: [])
         for file in self.tables:
             file_name, ext = os.path.splitext(file)
-            if ext=='.txt':
-                with open(os.path.join(BASE,file),'r') as f:
+            if ext == '.txt':
+                with open(os.path.join(BASE, file), 'r') as f:
                     self._data[file_name] = f.readlines()
-        
+            print('Read {} datapoints from {}'.format(
+                len(self._data[file_name]), file_name))
+
     def getItem(self, filename):
-        ret=random.choice(self._data[filename])
+        ret = random.choice(self._data[filename])
         return ret
 
+
 target = DataFetcher()
+
 
 def Extractor(keyword, null_token='O'):
     if '[' not in str(keyword):
         return [(tok.text, null_token) for tok in nlp(keyword)]
-    e = re.sub('[^0-9a-zA-Z]+',' ', str(keyword)).strip().split()
-    value=target.getItem(e[1]).split()
-    ret_tok=e[0]
+    e = re.sub('[^0-9a-zA-Z]+', ' ', str(keyword)).strip().split()
+    value = target.getItem(e[1]).split()
+    ret_tok = e[0]
     # [x for b in a for x in b]
-    ret=[(int_tok.text, 'I-'+ret_tok) if (ind+idx>0) else (int_tok.text, 'B-'+ret_tok) for ind, ent_tok in enumerate(value) for idx, int_tok in enumerate(nlp(ent_tok))]
+    ret = [(int_tok.text, 'I-' + ret_tok) if (ind + idx > 0) else
+           (int_tok.text, 'B-' + ret_tok) for ind, ent_tok in enumerate(value)
+           for idx, int_tok in enumerate(nlp(ent_tok))]
     return ret
 
+
 def process_string(query):
-    query=query.replace('\n','').split()
-    sentence=[]
-    labels=[]
+    query = query.replace('\n', '').split()
+    sentence = []
+    labels = []
     for token in query:
-        extracted=Extractor(token)
+        extracted = Extractor(token)
         for ent in extracted:
             sentence.append(ent[0])
             labels.append(ent[1])
-    res=[]
-    for i,j in zip(sentence, labels):
-        res.append(str(i)+'###'+str(j))
+    res = []
+    for i, j in zip(sentence, labels):
+        res.append(str(i) + '###' + str(j))
     return ' '.join(res)
-    
+
 
 def ArgParser():
     '''
@@ -66,35 +74,45 @@ def ArgParser():
     The script is compatible with python>3.5.2 
     '''
     parser = ArgumentParser()
-    parser.add_argument("Text_file_path", help = ".txt file relative path // in which templates are stored")
-    parser.add_argument("Output_file_path", help = "relative path of file where data is to be stored")
+    parser.add_argument(
+        "Text_file_path",
+        help=".txt file relative path // in which templates are stored")
+    parser.add_argument(
+        "Output_file_path",
+        help="relative path of file where data is to be stored")
     args = parser.parse_args()
 
     textPath = args.Text_file_path
     # Append a txt extention to the templates file if not specified
-    textPath = textPath+'.txt' if len(textPath.split('.')) == 1 else textPath 
+    textPath = textPath + '.txt' if len(textPath.split('.')) == 1 else textPath
 
     savePath = args.Output_file_path
     # Append a csv extention to the templates file if not specified
-    savePath = args.Output_file_path +'.csv' if len(savePath.split('.')) == 1 else args.Output_file_path
+    savePath = args.Output_file_path + '.csv' if len(
+        savePath.split('.')) == 1 else args.Output_file_path
 
     return textPath, savePath
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     textPath, savePath = ArgParser()
 
-    # Reading templates 
+    # Reading templates
     with open(textPath, 'r') as f:
         textData = f.readlines()
-    mode='a' if os.path.exists(savePath) else 'w'
-    header='data'
-    count=0
+    mode = 'a' if os.path.exists(savePath) else 'w'
+    header = 'data'
+    count = 0
     with open(savePath, mode) as out:
         for line in textData:
             tokens = line.split()
-            repeats = 1 if tokens[0].replace("{", "").replace("}", "")=='' else tokens[0].replace("{", "").replace("}", "")
-            query=' '.join(tokens[1:])
-            res=[]
+            repeats = 1 if tokens[0].replace("{", "").replace(
+                "}", "") == '' else tokens[0].replace("{", "").replace(
+                    "}", "")
+            query = ' '.join(tokens[1:])
+            res = []
             for _ in range(int(repeats)):
                 res.append(process_string(query))
-            out.write(res[0]+'\n')
+            out.write('\n'.join(res))
+            print('Done processing {} template'.format(count + 1))
+            count += 1
